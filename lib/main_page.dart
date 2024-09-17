85d22500801c458f9bf6e978e3ca63c00bbab5d0
zelk <zelk@users.noreply.github.com>
2024-09-17 14:08:29 +0200

    Created ZelkSearchableListView

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'widgets/zelk_searchable_list_view.dart';
 import 'models.dart';
 import 'routine_page.dart';
 
+// TODO: Make my own ListView that handles navigation and search bar
 // TODO: Handle mouse and keyboard interaction the way superhuman does
 // TODO: Click an item should use focus correctly
-// TODO: Make my own ListView that handles navigation and search bar
 // TODO: Check ideas below when needed:
 // DirectionalFocusTraversalPolicyMixin may be good with many columns where
 //   some rows are missing some
@@ -26,59 +26,14 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
-  final TextEditingController _searchController = TextEditingController();
-  int _listRowIndex = -1;
   late List<Routine> _allRoutines;
   late List<Routine> _filteredRoutines;
-  late List<List<FocusNode>> _listFocusNodes;
-  final FocusNode _searchFocusNode = FocusNode();
-  final FocusScopeNode _listFocusScopeNode = FocusScopeNode();
-  final int _numColumns = 3;
 
   @override
   void initState() {
     super.initState();
     _allRoutines = DummyDataGenerator.generateRoutines();
     _filteredRoutines = _allRoutines;
-    _searchController
-        .addListener(() => _filterRoutines(_searchController.text));
-    _listFocusNodes = List.generate(
-      _filteredRoutines.length,
-      (_) => List.generate(
-        _numColumns,
-        (_) => FocusNode(),
-      ),
-    );
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      FocusScope.of(context).requestFocus(_searchFocusNode);
-    });
-  }
-
-  @override
-  void dispose() {
-    _listFocusScopeNode.dispose();
-    _searchController.dispose();
-    _searchFocusNode.dispose();
-    for (var list in _listFocusNodes) {
-      for (var node in list) {
-        node.dispose();
-      }
-    }
-    super.dispose();
-  }
-
-  FocusNode _getListFocusNode(int row, int col) {
-    assert(row >= 0 && col >= 0);
-    if (row > _listFocusNodes.length - 1) {
-      // TODO: I will need to generate more than one. Check diff!!!
-      _listFocusNodes.add(List.generate(
-        _numColumns,
-        (_) => FocusNode(),
-      ));
-    }
-    // No dynamic column addition supported:
-    assert(col < _listFocusNodes[row].length);
-    return _listFocusNodes[row][col];
   }
 
   void _openRoutinePage(Routine routine) {
@@ -87,10 +42,7 @@ class MainPageState extends State<MainPage> {
       MaterialPageRoute(
         builder: (context) => RoutinePage(routine: routine),
       ),
-    ).then((_) {
-// TODO: Verify how this works with mouse navigation
-      _listFocusScopeNode.requestFocus();
-    });
+    );
   }
 
   void _filterRoutines(String value) {
@@ -102,171 +54,30 @@ class MainPageState extends State<MainPage> {
     });
   }
 
-  KeyEventResult _handleSearchFocusKeyPress(
-      BuildContext context, KeyEvent event) {
-    if ((event.logicalKey == LogicalKeyboardKey.enter ||
-            event.logicalKey == LogicalKeyboardKey.numpadEnter) &&
-        event is KeyDownEvent) {
-      if (_filteredRoutines.isNotEmpty) {
-        _listFocusScopeNode.requestFocus();
-        _getListFocusNode(0, 0).requestFocus();
-      }
-      return KeyEventResult.handled;
-    }
-    if (event.logicalKey == LogicalKeyboardKey.escape &&
-        (event is KeyDownEvent || event is KeyRepeatEvent)) {
-      if (_searchController.text.isNotEmpty) {
-        _searchController.clear();
-        _filterRoutines('');
-      } else {
-        Navigator.of(context).maybePop();
-      }
-      return KeyEventResult.handled;
-    }
-    if (event.logicalKey == LogicalKeyboardKey.arrowDown &&
-        (event is KeyDownEvent || event is KeyRepeatEvent)) {
-      if (_searchController.selection.baseOffset ==
-          _searchController.text.length) {
-        _getListFocusNode(0, 0).requestFocus();
-        return KeyEventResult.handled;
-      }
-    }
-    return KeyEventResult.ignored;
-  }
-
-  KeyEventResult _handleListFocusKeyPress(
-      BuildContext context, KeyEvent event) {
-    if ((event.logicalKey == LogicalKeyboardKey.enter ||
-            event.logicalKey == LogicalKeyboardKey.numpadEnter) &&
-        event is KeyDownEvent) {
-      assert(_filteredRoutines.isNotEmpty);
-      assert(_listRowIndex != -1);
-      _openRoutinePage(_filteredRoutines[_listRowIndex]);
-      return KeyEventResult.handled;
-    }
-    if (event.logicalKey == LogicalKeyboardKey.escape &&
-        (event is KeyDownEvent || event is KeyRepeatEvent)) {
-      _searchFocusNode.requestFocus();
-      FocusScope.of(context).focusedChild?.unfocus();
-      return KeyEventResult.handled;
-    }
-    if (event.logicalKey == LogicalKeyboardKey.arrowUp &&
-        (event is KeyDownEvent || event is KeyRepeatEvent)) {
-      if (_listRowIndex > 0) {
-        FocusScope.of(context)
-            .focusedChild
-            ?.focusInDirection(TraversalDirection.up);
-      } else {
-        _getListFocusNode(_listRowIndex, 0).unfocus();
-        _searchFocusNode.requestFocus();
-      }
-      return KeyEventResult.handled;
-    }
-    if (event.logicalKey == LogicalKeyboardKey.arrowDown &&
-        (event is KeyDownEvent || event is KeyRepeatEvent)) {
-      if (_listRowIndex < _filteredRoutines.length - 1) {
-        setState(() {
-          FocusScope.of(context)
-              .focusedChild
-              ?.focusInDirection(TraversalDirection.down);
-        });
-      }
-      return KeyEventResult.handled;
-    }
-    if (event.character != null &&
-        event.character!.isNotEmpty &&
-        event.logicalKey != LogicalKeyboardKey.tab &&
-        event.logicalKey != LogicalKeyboardKey.space) {
-      _getListFocusNode(_listRowIndex, 0).unfocus();
-      _searchFocusNode.requestFocus();
-      _searchController.text += event.character!;
-      return KeyEventResult.handled;
-    }
-
-    return KeyEventResult.ignored;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Routines{$_listRowIndex}'),
+        title: const Text('Routines'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FocusScope(
-              onKeyEvent: (node, event) {
-                return _handleSearchFocusKeyPress(context, event);
+      body: ZelkSearchableListView(
+          itemCount: _filteredRoutines.length,
+          filter: _filterRoutines,
+          onItemTap: (index) {
+            _openRoutinePage(_filteredRoutines[index]);
+          },
+          itemBuilder: (context, index, hasFocus) {
+            final routine = _filteredRoutines[index];
+            return ListTile(
+              title: Text(routine.name),
+              subtitle: Text('${routine.instances.length} instances'),
+// TODO: Where do I put this?
+              tileColor: hasFocus ? Colors.blue.withOpacity(0.5) : null,
+              onTap: () {
+                _openRoutinePage(routine);
               },
-              child: TextField(
-                autofocus: true,
-                controller: _searchController,
-                focusNode: _searchFocusNode,
-                decoration: InputDecoration(
-                  hintText: 'Search routines',
-                  hintStyle: TextStyle(color: Colors.grey[400]),
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: _searchController.text.isNotEmpty
-                      ? IconButton(
-                          icon: const Icon(Icons.clear),
-                          tooltip: 'Esc',
-                          onPressed: () {
-                            _searchController.clear();
-                            setState(() {});
-                          },
-                        )
-                      : null,
-                  border: const OutlineInputBorder(),
-                ),
-                onChanged: _filterRoutines,
-              ),
-            ),
-          ),
-          Expanded(
-            child: FocusScope(
-              node: _listFocusScopeNode,
-              onKeyEvent: (node, event) {
-                return _handleListFocusKeyPress(context, event);
-              },
-              child: _filteredRoutines.isEmpty
-                  ? const Center(
-                      child: Text('No matches'),
-                    )
-                  : ListView.builder(
-                      itemCount: _filteredRoutines.length,
-                      itemBuilder: (context, index) {
-                        final routine = _filteredRoutines[index];
-                        final focusNode = _getListFocusNode(index, 0);
-                        return ListTile(
-                          title: Text(routine.name),
-                          focusNode: focusNode,
-                          onFocusChange: (hasFocus) {
-                            if (hasFocus) {
-                              setState(() {
-                                _listRowIndex = index;
-                              });
-                            } else {
-                              if (_listRowIndex == index) {
-                                setState(() {
-                                  _listRowIndex = -1;
-                                });
-                              }
-                            }
-                          },
-                          subtitle:
-                              Text('${routine.instances.length} instances'),
-                          onTap: () {
-                            _openRoutinePage(routine);
-                          },
-                        );
-                      },
-                    ),
-            ),
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
---
+++ lib/widgets/zelk_searchable_list_view.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Added import
+
+// TODO: I PROBABLY NEED A LOT OF SETSTATE CALLS IN THIS WIDGET!!!
+
+// TODO: Consider if I should add <T> to the widget.
+class ZelkSearchableListView extends StatefulWidget {
+  final int itemCount;
+  final Function(String) filter;
+  final Function(int index) onItemTap;
+  final Widget Function(BuildContext context, int index, bool hasFocus)
+      itemBuilder;
+
+  const ZelkSearchableListView(
+      {super.key,
+      required this.itemCount,
+      required this.filter,
+      required this.onItemTap,
+      required this.itemBuilder});
+
+  @override
+  ZelkSearchableListViewState createState() => ZelkSearchableListViewState();
+}
+
+class ZelkSearchableListViewState extends State<ZelkSearchableListView> {
+  final TextEditingController _searchController = TextEditingController();
+  final FocusNode _searchFocusNode = FocusNode();
+  final FocusScopeNode _listFocusScopeNode = FocusScopeNode();
+  final int _numColumns = 3;
+  late List<List<FocusNode>> _listFocusNodes;
+  int _listRowIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() => widget.filter(_searchController.text));
+    _listFocusNodes = List.generate(
+      1, // TODO: Is it better to generate more from the get go?
+      // Actually, it might be best to do lazy initialization.
+      (_) => List.generate(
+        _numColumns,
+        (_) => FocusNode(),
+      ),
+    );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FocusScope.of(context).requestFocus(_searchFocusNode);
+    });
+  }
+
+  @override
+  void dispose() {
+    _listFocusScopeNode.dispose();
+    _searchController.dispose();
+    _searchFocusNode.dispose();
+    for (var list in _listFocusNodes) {
+      for (var node in list) {
+        node.dispose();
+      }
+    }
+    super.dispose();
+  }
+
+  FocusNode _getListFocusNode(int row, int col) {
+    assert(row >= 0 && col >= 0);
+    if (row > _listFocusNodes.length - 1) {
+      // TODO: I will need to generate more than one. Check diff!!!
+      _listFocusNodes.add(List.generate(
+        _numColumns,
+        (_) => FocusNode(),
+      ));
+    }
+    // No dynamic column addition supported:
+    assert(col < _listFocusNodes[row].length);
+    return _listFocusNodes[row][col];
+  }
+
+  KeyEventResult _handleSearchFocusKeyPress(
+      BuildContext context, KeyEvent event) {
+    if ((event.logicalKey == LogicalKeyboardKey.enter ||
+            event.logicalKey == LogicalKeyboardKey.numpadEnter) &&
+        event is KeyDownEvent) {
+      if (widget.itemCount > 0) {
+        _listFocusScopeNode.requestFocus();
+        _getListFocusNode(0, 0).requestFocus();
+      }
+      return KeyEventResult.handled;
+    }
+    if (event.logicalKey == LogicalKeyboardKey.escape &&
+        (event is KeyDownEvent || event is KeyRepeatEvent)) {
+      if (_searchController.text.isNotEmpty) {
+        _searchController.clear();
+        widget.filter('');
+      } else {
+        Navigator.of(context).maybePop();
+      }
+      return KeyEventResult.handled;
+    }
+    if (event.logicalKey == LogicalKeyboardKey.arrowDown &&
+        (event is KeyDownEvent || event is KeyRepeatEvent)) {
+      if (_searchController.selection.baseOffset ==
+          _searchController.text.length) {
+        _getListFocusNode(0, 0).requestFocus();
+        return KeyEventResult.handled;
+      }
+    }
+    return KeyEventResult.ignored;
+  }
+
+  KeyEventResult _handleListFocusKeyPress(
+      BuildContext context, KeyEvent event) {
+    if ((event.logicalKey == LogicalKeyboardKey.enter ||
+            event.logicalKey == LogicalKeyboardKey.numpadEnter) &&
+        event is KeyDownEvent) {
+      assert(widget.itemCount > 0);
+      assert(_listRowIndex != -1);
+      widget.onItemTap(_listRowIndex);
+      return KeyEventResult.handled;
+    }
+    if (event.logicalKey == LogicalKeyboardKey.escape &&
+        (event is KeyDownEvent || event is KeyRepeatEvent)) {
+      _searchFocusNode.requestFocus();
+      FocusScope.of(context).focusedChild?.unfocus();
+      return KeyEventResult.handled;
+    }
+    if (event.logicalKey == LogicalKeyboardKey.arrowUp &&
+        (event is KeyDownEvent || event is KeyRepeatEvent)) {
+      if (_listRowIndex > 0) {
+        FocusScope.of(context)
+            .focusedChild
+            ?.focusInDirection(TraversalDirection.up);
+      } else {
+        _getListFocusNode(_listRowIndex, 0).unfocus();
+        _searchFocusNode.requestFocus();
+      }
+      return KeyEventResult.handled;
+    }
+    if (event.logicalKey == LogicalKeyboardKey.arrowDown &&
+        (event is KeyDownEvent || event is KeyRepeatEvent)) {
+      if (_listRowIndex < widget.itemCount - 1) {
+        setState(() {
+          FocusScope.of(context)
+              .focusedChild
+              ?.focusInDirection(TraversalDirection.down);
+        });
+      }
+      return KeyEventResult.handled;
+    }
+    if (event.character != null &&
+        event.character!.isNotEmpty &&
+        event.logicalKey != LogicalKeyboardKey.tab &&
+        event.logicalKey != LogicalKeyboardKey.space) {
+      _getListFocusNode(_listRowIndex, 0).unfocus();
+      _searchFocusNode.requestFocus();
+      _searchController.text += event.character!;
+      return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("$_listRowIndex"),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FocusScope(
+            onKeyEvent: (node, event) {
+              return _handleSearchFocusKeyPress(context, event);
+            },
+            child: TextField(
+              autofocus: true,
+              controller: _searchController,
+              focusNode: _searchFocusNode,
+              decoration: InputDecoration(
+                hintText: 'Search routines',
+                hintStyle: TextStyle(color: Colors.grey[400]),
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: _searchController.text.isNotEmpty
+                    ? IconButton(
+                        icon: const Icon(Icons.clear),
+                        tooltip: 'Esc',
+                        onPressed: () {
+                          _searchController.clear();
+                          setState(() {});
+                        },
+                      )
+                    : null,
+                border: const OutlineInputBorder(),
+              ),
+              // TODO: Do I really need both this and the listener in initState?
+              //              onChanged: widget.filter(_searchController.text),
+            ),
+          ),
+        ),
+        Expanded(
+          child: FocusScope(
+            node: _listFocusScopeNode,
+            onKeyEvent: (node, event) {
+              return _handleListFocusKeyPress(context, event);
+            },
+            child: widget.itemCount == 0
+                ? const Center(
+                    child: Text('No matches'),
+                  )
+                : ListView.builder(
+                    itemCount: widget.itemCount,
+                    itemBuilder: (context, index) {
+                      final focusNode = _getListFocusNode(index, 0);
+                      return Focus(
+                          focusNode: focusNode,
+                          onFocusChange: (hasFocus) {
+                            if (hasFocus) {
+                              setState(() {
+                                _listRowIndex = index;
+                              });
+                            } else {
+                              if (_listRowIndex == index) {
+                                setState(() {
+                                  _listRowIndex = -1;
+                                });
+                              }
+                            }
+                          },
+                          child: ExcludeFocus(
+                            child: widget.itemBuilder(
+                                context, index, _listRowIndex == index),
+                          ));
+                    },
+                  ),
+          ),
+        ),
+      ],
+    );
+  }
+}
